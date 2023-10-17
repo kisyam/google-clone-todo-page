4b5b93cc384a89ed7e7f9dc96a411c3a26378f07
Kisyam <104766571+kisyam@users.noreply.github.com>
2023-10-17 11:04:59 +0900

    paintTodo

--- todo-form.js
+++ todo-form.js
@@ -2,9 +2,12 @@ const todoBar = document.getElementById("todo-bar");
 const addButton = document.getElementById("add-button");
 const todos = document.querySelector(".todos");
 
+function paintTodo(newTodo) {}
+
 function onAddToDos(event) {
     event.preventDefault();
-    const todo = todoBar.value;
+    const newTodo = todoBar.value;
+    paintTodo(newTodo);
     if (savedUsername === null) {
         handleLoginInput();
         return;
