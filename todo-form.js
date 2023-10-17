eafd87fd6b6885002513f0fcc0d6f3ceb55b9131
Kisyam <104766571+kisyam@users.noreply.github.com>
2023-10-17 23:37:22 +0900

    event

--- todo-form.js
+++ todo-form.js
@@ -1,9 +1,9 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
+const button = document.querySelector(".button");
 
 function paintTodo(newTodo) {
-    console.log("hi");
     const li = document.createElement("li");
     const span = document.createElement("span");
     li.appendChild(span);
@@ -13,10 +13,24 @@ function paintTodo(newTodo) {
 
 function onAddToDos(event) {
     event.preventDefault();
+    console.log(event.target.tagName);
+    console.log("this:", this.tagName);
     const newTodo = toDoInput.value;
-    console.log(newTodo);
     toDoInput.value = "";
     paintTodo(newTodo);
 }
+
+button.onclick = (event) => {
+    console.log("event.target.tagName:", event.target.tagName);
+    console.log("this:", this.tagName);
+};
+
+// button.onmouseenter = () => {
+//     alert("I find you!!!");
+// };
+
+// button.onmouseleave = () => {
+//     alert("See you!!");
+// };
 
 toDoForm.addEventListener("submit", onAddToDos);
